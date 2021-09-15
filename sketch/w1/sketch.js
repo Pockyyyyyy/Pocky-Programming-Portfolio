e4639f58a2c0e80d0fe04a1253fb8e5fdb0ef63f
Pockyyyyyy <70854036+Pockyyyyyy@users.noreply.github.com>
2021-09-15 19:53:18 -0400

    initial commit

--- sketch/w1/sketch.js
+++ sketch/w1/sketch.js
@@ -1,20 +1,53 @@
-let length = 40, size = 10, HEIGHT = 500, WIDTH = 500;
-
 function setup(){
-    createCanvas(WIDTH,HEIGHT);
+  createCanvas(800, 800);
 }
 
-function draw() {
-  background(0);
-  fill(255);
-  for (let y = 0; y < size; y++) {
-    for (let x = 0; x < size; x++) {
-      if ((x + y) % 2 != 0) {
-        let size2 = length+length*(mouseX+mouseY-HEIGHT)/HEIGHT;
-        square(200+x*length, 200+y*length, size2);
-        circle(200+x*length, 200+y*length, size2);
-        square(180+x*length, 180+y*length, size2);
-      }
-    }
-  }
+function draw(){
+
+  background(238,180,34);
+
+  fill(0,0,0);
+  ellipse(320,300,500,400);
+
+  fill(0,0,0);
+  ellipse(450,400,500,700);
+
+  translate(width / 2, height / 2);
+  rotate(PI / 180 * 25);
+  noStroke();
+  fill(255,228,196);
+  ellipse(-50,-50,400,500);
+
+  fill(0,0,0);
+  ellipse(-50,-210,300,200);
+
+  rotate(PI / 180 * 5);
+  fill(0,0,0);
+  ellipse(-180,-50,100,10);
+
+  rotate(PI / 180 * -35);
+  fill(0,0,0);
+  ellipse(70,-80,100,10);
+
+  rotate(PI / 180 * 0);
+  fill(0,0,0);
+  ellipse(-130,-100,50,50);
+
+  rotate(PI / 180 * 0);
+  fill(0,0,0);
+  ellipse(60,-40,50,50);
+
+  rotate(PI / 180 * 10);
+  fill(221,181,148);
+  rect(-90,-50,50,130);
+
+  rotate(PI / 180 * 10);
+  fill(255,218,185);
+  rect(-90,-50,50,130);
+
+  rotate(PI / 180 * -20);
+  fill(178,34,34);
+  circle(-70,110,30,30);
+
 }
+
